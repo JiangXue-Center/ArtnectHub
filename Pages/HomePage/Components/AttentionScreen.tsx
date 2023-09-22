79b17b528a2cb269b136e5a5a27b72a7ac3c4307
wuyujin <3075385315@qq.com>
2023-09-22 15:25:04 +0800

    添加了一些首页细节

--- Pages/HomePage/Components/AttentionScreen.tsx
+++ Pages/HomePage/Components/AttentionScreen.tsx
@@ -10,21 +10,19 @@ const AttentionScreen = () => {
         <SafeAreaView>
             <ScrollView>
                 <AttentionSwiper/>
-                <View style={{flex: 2, flexDirection: "row",flexWrap: "wrap",alignItems: "center",justifyContent: "center"}}>
-                    {cardStore.map((item) => (
-                        <Box borderWidth="1" borderRadius="md" width={Dimensions.get("window").width/2.2} margin={2}>
-                            <VStack divider={<Divider />} height={250} key={item.key}>
+                <View style={{
+                    flex: 2,
+                    flexDirection: "row",
+                    // columnGap: 2,
+                    flexWrap: "wrap",
+                    alignItems: "center",
+                    justifyContent: "center"
+                }}>
+                    {cardStore.map((item) => (<Box borderWidth="1" borderRadius="md" width={Dimensions.get("window").width / 2.2} margin={2}>
+                            <VStack divider={<Divider/>} height={250} key={item.key}>
                                 <Box>
-                                    <Image resizeMode="contain" size={100} height={200} width={Dimensions.get("window").width/2.1} source={{uri: item.sources}} />
-                                </Box>
-                                <Box px="4" pb="4">
-                                    GeekyAnts
-                                </Box>
-                            </VStack>
-
-                            <VStack divider={<Divider />} height={200} key={item.key}>
-                                <Box>
-                                    <Image resizeMode="contain" size={100} height={150} width={Dimensions.get("window").width/2.1} source={{uri: item.sources}} />
+                                    <Image resizeMode="contain" size={100} height={200}
+                                           width={Dimensions.get("window").width / 2.1} source={{uri: item.sources}}/>
                                 </Box>
                                 <Box px="4" pb="4">
                                     GeekyAnts
